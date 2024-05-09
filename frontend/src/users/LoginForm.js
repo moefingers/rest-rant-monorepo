94aee8cb89d62eb9d6b921d73ce58247cf413c40
MoeFingers <mbzuiter@gmail.com>
2024-05-08 21:07:24 -0600

    added jwts

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,25 +17,41 @@ function LoginForm() {
 
     
   
+
+
+    
     async function handleSubmit(e) {
-        console.log(credentials)
-        const response = await fetch(`http://localhost:5000/authentication/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials)
-        })
+        e.preventDefault();
     
-        const data = await response.json()
+        try {
+            console.log(credentials)
+            const response = await fetch(`http://localhost:5000/authentication/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(credentials)
+            });
     
-        if (response.status === 200) {
-            setCurrentUser(data.user)
-            history.push(`/`)
-        } else {
-            setErrorMessage(data.message)
+            const data = await response.json();
+    
+        
+            if (response.ok) {  
+                setCurrentUser(data.user);
+                localStorage.setItem('token', data.token);
+                history.push(`/`);
+            } else {
+                setErrorMessage(data.message);
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+            setErrorMessage('An error occurred, please try again');
         }
     }
+     
+     
+     
+     
       
     
   
